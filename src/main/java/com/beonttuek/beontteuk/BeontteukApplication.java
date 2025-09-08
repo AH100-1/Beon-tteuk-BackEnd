e4c24c64e4005ca2a6b0e59aae179d29b3a83ff4
pswaao88 <pswaao88@gmail.com>
2025-09-08 20:28:21 +0900

    init: 프로젝트 시작

---
+++ src/main/java/com/beonttuek/beontteuk/BeontteukApplication.java
@@ -0,0 +1,13 @@
+package com.beonttuek.beontteuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeontteukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeontteukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beonttuek/beontteuk/BeontteukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beonttuek.beontteuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeontteukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

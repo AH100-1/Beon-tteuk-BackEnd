aff6e9b7a5646f1f36d9300ea50b6d4f3c25dd1b
pswaao88 <pswaao88@gmail.com>
2025-10-07 16:15:08 +0900

    feat: 공통 과목 엔티티 클래스 생성

--- src/main/java/com/beontteuk/beontteuk/study/subject/entity/CommonSubject.java
+++ src/main/java/com/beontteuk/beontteuk/study/subject/entity/CommonSubject.java
@@ -1,5 +1,41 @@
 package com.beontteuk.beontteuk.study.subject.entity;
 
-public class CommonSubject {
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "common_subject")
+public class CommonSubject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subject_common_id")
+    private Long subjectCommonId;
+
+    @Column(name = "subject_name", nullable = false)
+    private String subjectName;
+
+    @Lob
+    @Column(name = "content")
+    private byte[] content;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public CommonSubject(String subjectName, byte[] content) {
+        this.subjectName = subjectName;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+    }
 }
